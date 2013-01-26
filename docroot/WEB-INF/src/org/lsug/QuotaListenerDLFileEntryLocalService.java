a6f3f288ff00d37d3928358f1ebb533451791e42
Juan Fernández <juan.fernandez@liferay.com>
2013-01-26 11:41:27 +0100

    #4 DLFileEntry service wrapper all public delete methods + temporary info messages

--- docroot/WEB-INF/src/org/lsug/QuotaListenerDLFileEntryLocalService.java
+++ docroot/WEB-INF/src/org/lsug/QuotaListenerDLFileEntryLocalService.java
@@ -4,8 +4,8 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.portlet.documentlibrary.model.DLFileEntry;
-import com.liferay.portlet.documentlibrary.service.DLFileEntryLocalServiceWrapper;
 import com.liferay.portlet.documentlibrary.service.DLFileEntryLocalService;
+import com.liferay.portlet.documentlibrary.service.DLFileEntryLocalServiceWrapper;
 import com.liferay.portlet.dynamicdatamapping.storage.Fields;
 
 import java.io.File;
@@ -31,21 +31,57 @@ public class QuotaListenerDLFileEntryLocalService
 		
 		// Add quota control here
 
-		return super.addFileEntry(
+		System.out.println("Controlando quota excedida - file size: " + size);
+
+		DLFileEntry dlFileEntry = super.addFileEntry(
 			userId, groupId, repositoryId, folderId, sourceFileName, mimeType, 
 			title, description, changeLog, fileEntryTypeId, fieldsMap, file, is,
 			size, serviceContext);
 		
 		// Update consumed quota here
+
+		System.out.println("Actualizando quota - file size: " + size);
+		
+		return dlFileEntry;
 	}
 
-	
 	protected void deleteFileEntry(DLFileEntry dlFileEntry)
 			throws PortalException, SystemException {
 		
 		super.deleteDLFileEntry(dlFileEntry);
 		
 		// Update consumed quota here
+		
+		System.out.println("Actualizando quota - file size: " + 
+			dlFileEntry.getSize());
+	}
+	
+	public void deleteFileEntry(long fileEntryId)
+		throws PortalException, SystemException {
+
+		DLFileEntry dlFileEntry = super.getFileEntry(fileEntryId);
+
+		long size = dlFileEntry.getSize();
+
+		super.deleteDLFileEntry(fileEntryId);
+
+		// Update consumed quota here
+
+		System.out.println("Actualizando quota - file size: " + size);
+	}
+
+	public void deleteFileEntry(long userId, long fileEntryId)
+		throws PortalException, SystemException {
+
+		DLFileEntry dlFileEntry = super.getFileEntry(fileEntryId);
+
+		long size = dlFileEntry.getSize();
+
+		super.deleteFileEntry(userId, fileEntryId);
+		
+		// Update consumed quota here
+
+		System.out.println("Actualizando quota - file size: " + size);
 	}
 
 }
