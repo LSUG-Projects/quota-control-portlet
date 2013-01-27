2a4b69f677c167432450a26562e81054d8f8bf44
Borxa Varela Bouzas <borxa@siretec.com>
2013-01-27 10:53:00 +0100

    Add jsp files, portlet class and languague keys for sites quota

---
+++ docroot/WEB-INF/src/org/lsug/quota/portlet/SitesQuotaPortlet.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2013 Liferay Spain User Group All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package org.lsug.quota.portlet;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.util.bridges.mvc.MVCPortlet;
+import org.lsug.quota.model.Quota;
+import org.lsug.quota.service.QuotaServiceUtil;
+
+public class SitesQuotaPortlet extends MVCPortlet {
+	
+	public Quota updateQuota(ActionRequest actionRequest, ActionResponse actionResponse) 
+			throws Exception {
+		
+		long quotaId = ParamUtil.getLong(actionRequest, "quotaId");
+		long classNameId = ParamUtil.getLong(actionRequest, "classNameId");
+	    long classPK = ParamUtil.getLong(actionRequest, "classPK");
+		int quotaAlert = ParamUtil.getInteger(actionRequest, "quotaAlert");
+		long quotaAssigned = ParamUtil.getLong(actionRequest, "quotaAssigned");
+		long quotaUsed = ParamUtil.getLong(actionRequest, "quotaUsed");
+		
+		return QuotaServiceUtil.updateQuota(quotaId, classNameId, classPK, 
+				quotaAlert, quotaAssigned, quotaUsed);
+	}
+
+}
